afcd43e19871dc1c07070abbfdf8031011f59993
Zacgoose <107489668+Zacgoose@users.noreply.github.com>
2026-01-05 18:46:22 +0800

    Update Invoke-Admin2fatokensetup.ps1

--- Modules/PrivateApi/Public/Invoke-Admin2fatokensetup.ps1
+++ Modules/PrivateApi/Public/Invoke-Admin2fatokensetup.ps1
@@ -86,7 +86,7 @@ function Invoke-Admin2fatokensetup {
                     # Don't enable yet - user needs to verify it works first
                     # That will happen in a separate "enable" action
                     
-                    Add-LinkToMeAzDataTableEntity @UsersTable -Entity $UserRecord -Force
+                    Add-LinkToMeAzDataTableEntity @UsersTable -OperationType 'UpsertMerge' -Entity $UserRecord
                     
                     $Response.totp = @{
                         secret = $TotpSecret
